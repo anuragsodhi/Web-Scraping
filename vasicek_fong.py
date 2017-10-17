f4a30f02be30220a7031b19f509955f841419c10
Anurag Sodhi <anuragsodhi@gmail.com>
2017-10-17 03:51:41 -0400

    Bond pricing using Vasicek fong
    Added pricing function using Vasicek fong

--- vasicek_fong.py
+++ vasicek_fong.py
@@ -6,6 +6,8 @@ Created on Mon Oct  2 00:41:14 2017
 """
 
 import math
+#from datetime import datetime
+import QuantLib as ql
 
 def vf_discount_function(parameters,knots,t):
     sum_df = 1
@@ -23,5 +25,61 @@ def vf_discount_function(parameters,knots,t):
                                     + (1/3)*(1-math.e**(-3*a*(t-k_t))))
     return(sum_df)
     
-parameters,knots,t = ([0.0282,-0.7587,-1.1631,0.8958,-2.2002,1.0815,1.2169,0.5667,-0.5866], [0.5,2,5,10,30], 5)    
+parameters = [0.0304,-0.5577,-0.8063,0.5342,-2.5088,0.6706,0.7541,0.3981,0.1756]
+knots = [0.5,2,5,10,30]
+t=2
+vf_discount_function(parameters,knots,t)
+
+## Input settlement date in 'mm/dd/yyyy' format
+settlement_date = '10/3/2017'
+next_coupon = ['2/28/2018','1/31/2018', '2/15/2018']
+maturity_date = ['8/31/2019', '7/31/2020', '8/15/2028']
+
+#sd  = datetime.strptime(settlement_date, '%m/%d/%Y')
+
+sd_split = list(map(int,settlement_date.split('/')))
+sd = ql.Date(sd_split[1] , sd_split[0] , sd_split[2])
+
+tenor = ql.Period(ql.Semiannual)
+calendar = ql.UnitedStates()
+
+cp=[1, 1.625, 5.5]
+p =100
+
+for i, maturity in enumerate(maturity_date):
+    md_split = list(map(int,maturity_date[i].split('/')))
+    md = ql.Date(md_split[1] , md_split[0] , md_split[2])
+    schedule = ql.Schedule(sd, md, tenor, calendar, ql.Following,
+                           ql.Following, ql.DateGeneration.Backward, False)
+    k =list(schedule)
+    t=0
+    c=0
+    for j in range(1,len(k)):
+        if j==1:
+            t = t + (k[j] - k[j-1])/360
+        else:
+            t = t + 181/360
+        df = vf_discount_function(parameters,knots,t)
+        c= c + (cp[i]/2)*df
+        if j == len(k) -1:
+            c = c + p*df
+    print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 vf_discount_function(parameters,knots,t)
