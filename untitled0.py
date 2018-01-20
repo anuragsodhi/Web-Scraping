81936da838816ac40eff3d9b01da0edf08bf82af
Anurag Sodhi <anuragsodhi@gmail.com>
2018-01-20 15:36:07 -0500

    Coupon cabin

--- untitled0.py
+++ untitled0.py
@@ -4,7 +4,8 @@ Created on Tue Sep 26 00:20:14 2017
 
 @author: Anurag
 """
-
+import os
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
@@ -13,25 +14,33 @@ import pandas as pd
 driver = webdriver.Chrome(executable_path='C:/Users/Anurag/Downloads/chromedriver_win32/chromedriver.exe')
 search_keyword= ['exercise equipment']
 
-##start loop for all search items
-for keyword in search_keyword:
-    driver.get("http://www.amazon.com")
-    inputElement = driver.find_element_by_name("field-keywords")
-    inputElement.send_keys(keyword)
-    inputElement.send_keys(Keys.RETURN)
-    sauce = driver.page_source
-    soup = BeautifulSoup(sauce, "lxml")
-    links_soup = soup.find_all("a", class_="a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal")    
-    links=[]
-    for i in links_soup:
-        if i.get("href")[0:4] == "http":
-            links.append(i.get("href"))
-        else:
-            links.append("https://www.amazon.com"+i.get("href"))
-    product=[]
-    price =[]
-    weight=[]        
-    for link in links:
+driver.get("https://www.couponcabin.com/coupons/express/#membersonly-3606563")
+           
+inputElement = driver.find_element_by_xpath("//span[@class='countdown']")
+i=0
+while(i<10):
+    print(inputElement.text)
+    time.sleep(1)
+    i+=1
+driver.get("https://www.couponcabin.com/coupons/joann-fabric/#membersonly-3598407")
+inputElement = driver.find_element_by_xpath("//a[@href='#']")    
+driver.page_source
+inputElement.send_keys(keyword)
+inputElement.send_keys(Keys.RETURN)
+sauce = driver.page_source
+soup = BeautifulSoup(sauce, "lxml")
+links_soup = soup.find_all("a", class_="a-link-normal s-access-detail-page s-color-twister-title-link a-text-normal")    
+links=[]
+for i in links_soup:
+    if i.get("href")[0:4] == "http":
+        links.append(i.get("href"))
+    else:
+        links.append("https://www.amazon.com"+i.get("href"))
+product=[]
+price =[]
+weight=[]        
+for link in links:
+    try:
         driver.get(link)
         product.append(driver.find_element_by_id("productTitle").text)
         try:
@@ -44,9 +53,11 @@ for keyword in search_keyword:
         else:
             weight.append(shp[shp.find('Shipping Weight:')+len('Shipping Weight:')+1:shp.find('(View shipping rates and policies)')-1])
         print('done one of the links')
-    
-    final_list = list(zip(product,price,weight))
-    df = pd.DataFrame(final_list)
-    df.to_excel('output.xlsx', header=False, index=False)
-    print('Finished!')
-    
+    except:
+        pass
+
+final_list = list(zip(product,price,weight))
+df = pd.DataFrame(final_list)
+df.to_excel('output.xlsx', header=False, index=False)
+print('Finished!!')
+
